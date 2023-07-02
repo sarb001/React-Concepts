afde901022701177e242ced4807b1379759aa69f
SARB <mrsinghbusiness05@gmail.com>
2023-07-02 11:49:25 +0530

    started - api-call-with-toolkit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import Cart from './Reduxjs-Toolkit/Cart';
-import Products from './Reduxjs-Toolkit/Products';
-import products  from './products.json';
 
 function App() {
 
   return (
     <div className="App">
           <h2>   Main App  </h2>
-          <h1> Building Cart with Reduxjs-Toolkit  </h1>
-              <Cart />
           <div className="text-center">
-            {products.map((product) => (
-              <Products  {...product} />
-              ))}
+              <h1> Building Cart with Reduxjs-Toolkit  </h1>
             </div>
     </div>
 
